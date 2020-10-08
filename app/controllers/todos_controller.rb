e6729e253bf221c5fb15fc56f2abb2962bb89169
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-10-08 12:31:42 +0200

    Remove the show page
    This patch removes the `show.html.erb` view. Accessing a single todo now
    redirects to the `root`, while still keeping the `GET /todos/1.json`
    JSON response format support.
    
    Both the `create` and `update` controller actions have been updated to
    redirect to the `root` URL directly as well.
    
    More info about responding to different format:
    - https://api.rubyonrails.org/classes/ActionController/MimeResponds.html#method-i-respond_to
    - https://api.rubyonrails.org/classes/ActionController/Redirecting.html#method-i-redirect_to

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,9 +7,12 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
-  # GET /todos/1
   # GET /todos/1.json
   def show
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.json
+    end
   end
 
   # GET /todos/new
@@ -28,7 +31,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
@@ -42,7 +45,7 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.html { redirect_to root_url, notice: 'Todo was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit }
