ee329ec98f5bfb005c7e324b24ebbbac8d19a355
sebastienvial <sebastien.vial68@gmail.com>
2020-11-12 18:11:58 +0000

    commit Exercice-02

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,6 +9,10 @@ class TodosController < ApplicationController
 
   # GET /todos/1.json
   def show
+     respond_to do |format|
+        format.html { redirect_to root_url, notice: 'Todo was successfully edit.' }
+        format.json  
+     end
   end
 
   # GET /todos/new
