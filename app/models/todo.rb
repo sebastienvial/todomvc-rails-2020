d2f3c3f5de167f83274980a876895b8758eea23e
sebastienvial <sebastien.vial68@gmail.com>
2020-11-18 19:30:10 +0000

    Exercise-04

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,8 @@
 class Todo < ApplicationRecord
-  # TODO add title presence validation
-  # TODO trim title
+  validates :title, presence:true
+
+  def title=(string)
+    super string.strip
+  end
+  
 end
